3145565bd5f05c21f49cede392cf728c7fb8efef
ignacioborraz <ignacioborraz@hotmail.com>
2023-02-21 18:40:17 -0300

    effects

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import './App.css'
 import LayoutMain from './layouts/LayoutMain/LayoutMain'
 import CardEvent from './components/CardEvent/CardEvent'
 import ButtonIndex from './components/ButtonIndex/ButtonIndex'
-import { data } from './data'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 
 function App() {
 
@@ -11,6 +10,17 @@ function App() {
 	/**
 	 * @render parámetro para leer el estado (que inicia en false: y no mrenderiza los eventos)
 	 * @setRender funcion que modifica el estado (por convencion se escribe la palabra set seguido de camelCase el estado que quiere modificar)
+	 */
+	let [data,setData] = useState({})
+	useEffect(
+		()=> {fetch('./data.json').then(res => res.json()).then(res => setData(res))},
+		[]
+	)
+	/**
+	 * @funcion que necesita ser controlada (en este caso una peticion que carga el estado con los datos de los eventos)
+	 * @array que contiene todos los parámetros que al cambiar (como render de true a false y al revés) ejecutan el efecto (la petición)
+	 * en este caso en particular no es necesario un parámetro de ejecución del efecto ya que se va a realizar la petición cada vez que se monte
+	 * en el caso del carousel que nunca se desmonta, si o si, necesito un parámetro que ejecute la petición para obtener los datos de la imagen siguiente
 	 */
 
 	const handleRender = ()=> {
